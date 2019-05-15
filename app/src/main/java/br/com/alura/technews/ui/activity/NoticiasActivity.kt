d5458ca4c9bc9245ea42ec2d24c98d99e21eb4d6
Alex Felipe <alexfelipevieira@gmail.com>
2019-05-15 10:53:00 -0300

    4.1

--- app/src/main/java/br/com/alura/technews/ui/activity/NoticiasActivity.kt
+++ app/src/main/java/br/com/alura/technews/ui/activity/NoticiasActivity.kt
@@ -23,7 +23,7 @@ class NoticiasActivity : AppCompatActivity() {
 
     private fun abreListaNoticias() {
         transacaoFragment {
-            replace(R.id.activity_noticias_container, ListaNoticiasFragment())
+            replace(R.id.activity_noticias_container_primario, ListaNoticiasFragment())
         }
     }
 
@@ -61,7 +61,7 @@ class NoticiasActivity : AppCompatActivity() {
         fragment.arguments = dados
         transacaoFragment {
             addToBackStack(null)
-            replace(R.id.activity_noticias_container, fragment)
+            replace(R.id.activity_noticias_container_secundario, fragment)
         }
     }
 
