216faf86f41bb89ffc0f1fdc646edefee1889af7
Alex Felipe <alexfelipevieira@gmail.com>
2019-05-16 10:13:52 -0300

    4.3

--- app/src/main/java/br/com/alura/technews/ui/activity/NoticiasActivity.kt
+++ app/src/main/java/br/com/alura/technews/ui/activity/NoticiasActivity.kt
@@ -12,13 +12,39 @@ import br.com.alura.technews.ui.activity.extensions.transacaoFragment
 import br.com.alura.technews.ui.fragment.ListaNoticiasFragment
 import br.com.alura.technews.ui.fragment.VisualizaNoticiaFragment
 
+private const val TAG_FRAGMENT_VISUALIZA_NOTICIA = "visualizaNoticia"
+
 class NoticiasActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_noticias)
-        if(savedInstanceState == null){
+        if (savedInstanceState == null) {
             abreListaNoticias()
+        } else {
+            supportFragmentManager
+                .findFragmentByTag(TAG_FRAGMENT_VISUALIZA_NOTICIA)?.let { fragment ->
+
+                    val argumentos = fragment.arguments
+                    val novoFragment = VisualizaNoticiaFragment()
+                    novoFragment.arguments = argumentos
+
+                    transacaoFragment {
+                        remove(fragment)
+                    }
+                    supportFragmentManager.popBackStack()
+
+                    transacaoFragment {
+                        val container =
+                            if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                                R.id.activity_noticias_container_secundario
+                            } else {
+                                addToBackStack(null)
+                                R.id.activity_noticias_container_primario
+                            }
+                        replace(container, novoFragment, TAG_FRAGMENT_VISUALIZA_NOTICIA)
+                    }
+                }
         }
     }
 
@@ -30,7 +56,7 @@ class NoticiasActivity : AppCompatActivity() {
 
     override fun onAttachFragment(fragment: Fragment?) {
         super.onAttachFragment(fragment)
-        when(fragment){
+        when (fragment) {
             is ListaNoticiasFragment -> {
                 configuraListaNoticias(fragment)
             }
@@ -61,13 +87,13 @@ class NoticiasActivity : AppCompatActivity() {
         dados.putLong(NOTICIA_ID_CHAVE, noticia.id)
         fragment.arguments = dados
         transacaoFragment {
-            val container = if(resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE){
+            val container = if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
                 R.id.activity_noticias_container_secundario
             } else {
                 addToBackStack(null)
                 R.id.activity_noticias_container_primario
             }
-            replace(container, fragment)
+            replace(container, fragment, TAG_FRAGMENT_VISUALIZA_NOTICIA)
         }
     }
 
